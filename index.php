8a8d0b96a12cdd05c9d27d1e5e3021ab1dd9abd8
PurpleMAD <PurpleMAD@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-10 08:44:50 +0000

    Old Version
    git-svn-id: https://plugins.svn.wordpress.org/manage-issue-based-magazine/tags/1.1@986206 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,186 @@
+<?php
+/*
+Plugin Name: Magazine Issue Manager
+Plugin URI: http://mim.purplemadprojects.com/
+Description: To manage issue based online publication content. Using this plugin site owner can publish cotent in issues using either WordPress categorized article features or in form of PDFs. Site owner can plan content in terms of periodic issues and users can browse content by selecting issues on website.
+Version: 1.1
+Text Domain: mim-issue
+Author: PurpleMAD
+Author URI: http://www.purplemad.ca/
+*/
+
+define( 'MIM_PLUGIN_URL', 			plugin_dir_url( __FILE__ ) );
+define( 'MIM_PLUGIN_PATH',			plugin_dir_path( __FILE__ ) );
+define( 'MIM_PLUGIN_BASENAME', 		plugin_basename( __FILE__ ) );
+define( 'MIM_PLUGIN_REL_DIR', 		dirname( PLUGIN_BASENAME ) );
+
+/**
+* When plugin loaded then all files called.
+*
+* Function Name: mim_issue_plugin_load_function.
+*
+* @created by {Nilesh Mokani} and {01-12-2013}
+*
+**/
+
+add_action( 'plugins_loaded','mim_issue_plugin_load_function' );
+function mim_issue_plugin_load_function(){
+	require_once( 'mim-issue-class.php' );	
+	
+	if (class_exists( 'MIM_Issue' ) ) {		
+		$mim_issue=new MIM_Issue();
+		require_once( 'mim-custom-class.php' );
+		$mim_custom=new MIM_Custom();
+				
+		require_once( 'mim-posttype-taxonomy.php' );
+		$current_user = wp_get_current_user();
+		if($current_user->roles[0] != 'editor')
+		{
+			require_once( 'mim-user-field.php' );
+		}
+		require_once('mim-restriction-editors.php');
+		require_once('mim-shortcode.php');
+
+	}
+	
+	# Load the language files
+	
+	load_plugin_textdomain( 'mim-issue', false, plugin_basename( dirname( __FILE__ )  . '/languages/' ));
+	
+}
+
+register_activation_hook( __FILE__, 'min_activate' );	
+register_deactivation_hook( __FILE__,'min_deactivate' );
+register_uninstall_hook( __FILE__, 'mim_uninstall'  );
+
+/**
+*  Activate Plugin : When activate plugin then tabel created and default value added in database.
+*
+* Function Name: min_activate
+*
+* @created by {Nilesh Mokani} and {01-12-2013}
+*
+**/
+
+function min_activate(){			
+	global $wpdb,$wp_query;
+	$charset_collate = '';	
+	if ( ! empty($wpdb->charset) )
+		$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+	if ( ! empty($wpdb->collate) )
+		$charset_collate .= " COLLATE $wpdb->collate";
+	
+	
+	$tables = $wpdb->get_results("show tables like '{$wpdb->prefix}taxonomymeta'");
+	if (!count($tables)):
+		$wpdb->query("CREATE TABLE {$wpdb->prefix}taxonomymeta (
+			meta_id bigint(20) unsigned NOT NULL auto_increment,
+			taxonomy_id bigint(20) unsigned NOT NULL default '0',
+			meta_key varchar(255) default NULL,
+			meta_value longtext,
+			PRIMARY KEY	(meta_id),
+			KEY taxonomy_id (taxonomy_id),
+			KEY meta_key (meta_key)
+		) $charset_collate;");
+	endif;	
+	
+		add_option('mim_full_article_display','No','', 'yes');
+		add_option('mim_allow_user_post_article','No','', 'yes');
+		add_option('mim_default_post_article_status','draft','', 'yes');
+		update_option('users_can_register','1');
+		update_option('default_role','editor');
+		update_option('posts_per_page','5');
+		add_option('mim_new_editor_status','Yes','', 'yes');
+		add_option('mim_cover_width','850','', 'yes');
+		add_option('mim_cover_height','450','', 'yes');
+		add_option('mim_post_per_page_article','5','', 'yes');
+		add_option('mim_search_behaviour','Yes','', 'yes');		
+		add_option('mim_current_issue','-1','', 'yes');		
+		
+		require_once( 'mim-custom-class.php' );
+		$mim_custom=new MIM_Custom();
+		$mim_custom->mim_load_post_type_taxonomy();
+		$issue_term = wp_insert_term(
+			  __('Default Issue','mim-issue'), // the term 
+			  'issues', // the taxonomy
+			  array(
+			    'description'=> __('This is Default Issue.','mim-issue'),
+			    'slug' => __('default-issue','mim-issue'),
+			    'parent'=> ''
+			  )
+			);
+			
+			
+			$magazine_term = wp_insert_term(
+			  __('Default Magazine Category','mim-issue'), // the term 
+			  'magazine_category', // the taxonomy
+			  array(
+			    'description'=>  __('This is Default Magazine Category.','mim-issue'),
+			    'slug' => __('default-magazine','mim-issue'),
+			    'parent'=> ''
+			  )
+			);
+	
+}
+
+/**
+*  Deactivate Plugin : When deactivate plugin then default value deleted in database.
+*
+* Function Name: min_deactivate
+*
+* @created by {Nilesh Mokani} and {01-12-2013}
+*
+**/
+	 
+function min_deactivate(){
+	delete_option('mim_full_article_display');
+	delete_option('mim_allow_user_post_article');
+	delete_option('mim_default_post_article_status');
+	delete_option('mim_new_editor_status');
+	delete_option('mim_cover_width');
+	delete_option('mim_cover_height');
+	delete_option('mim_post_per_page_article');
+	delete_option('mim_search_behaviour');
+	delete_option('mim_current_issue');
+}
+	
+/**
+*  Uninstall Plugin : When uninstall plugin then tabel droped and default value deleted in database.
+*
+* Function Name: uninstall
+*
+* @created by {Nilesh Mokani} and {01-12-2013}
+*
+**/
+		
+function mim_uninstall(){
+	global $wpdb;
+	$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}taxonomymeta");
+	
+	delete_option('mim_full_article_display');
+	delete_option('mim_allow_user_post_article');
+	delete_option('mim_default_post_article_status');
+	delete_option('mim_new_editor_status');
+	delete_option('mim_cover_width');
+	delete_option('mim_cover_height');
+	delete_option('mim_post_per_page_article');
+	delete_option('mim_search_behaviour');
+	delete_option('mim_issue_menu_type');
+	delete_option('mim_current_issue');
+}
+
+/**
+* Taxonomymeta tabel assign to golbal.
+*
+* Function Name: wpdbfix
+*
+* @created by {Nilesh Mokani} and {01-12-2013}
+*
+**/
+
+add_action( 'init', 'wpdbfix');
+function wpdbfix() {
+	global $wpdb;
+	$wpdb->taxonomymeta = "{$wpdb->prefix}taxonomymeta";
+}
+?>
---
+++ mim-user-field.php
@@ -0,0 +1,210 @@
+<?php
+/**
+* User Status change only administartor.
+*
+* Function Name: mim_custom_user_profile_fields.
+*
+* @created by {Nilesh Mokani} and {12-12-2013}
+*
+**/
+function mim_custom_user_profile_fields( $user ) {
+	
+	if($user->roles[0]!='administrator'){
+?>
+	<h3><?php _e('User Status Updating', 'mim-issue'); ?></h3>
+	
+	<table class="form-table">
+		<tr>
+			<th><label for="user_status"><?php _e('User Status', 'mim-issue'); ?></label></th>
+			<td>
+				<?php $user_status_arr=array('approved'=>'Approved','pending'=>'Pending');
+				 	  $curr_status=esc_attr(get_the_author_meta('mim_user_status', $user->ID) ); 
+				?>
+				
+				<select name="mim_user_status">
+					<?php foreach($user_status_arr as $key=>$value){?>
+						<option value="<?php _e($key);?>" <?php selected($curr_status,$key,true); ?>><?php _e($value);?></option>
+					<?php } ?>
+				</select>
+			</td>
+		</tr>
+	</table>
+<?php
+	} 
+}
+
+/**
+* Save User Status change only administartor.
+*
+* Function Name: mim_save_custom_user_profile_fields.
+*
+* @created by {Nilesh Mokani} and {12-12-2013}
+*
+**/
+function mim_save_custom_user_profile_fields( $user_id ) {
+	
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return FALSE;
+	
+	update_usermeta( $user_id, 'mim_user_status', $_POST['mim_user_status'] );
+}
+
+add_action( 'show_user_profile', 'mim_custom_user_profile_fields' );
+add_action( 'edit_user_profile', 'mim_custom_user_profile_fields' );
+
+add_action( 'personal_options_update', 'mim_save_custom_user_profile_fields' );
+add_action( 'edit_user_profile_update', 'mim_save_custom_user_profile_fields' );
+
+/**
+* When user register then change default user status.
+*
+* Function Name: add_user_status.
+*
+* @created by {Nilesh Mokani} and {12-12-2013}
+*
+**/
+
+add_action( 'user_register', 'add_user_status' );
+function add_user_status( $user_id ) {
+		$mim_curr_sel_new_editor_val=get_option('mim_new_editor_status');	
+		if($mim_curr_sel_new_editor_val == 'approved')
+		{
+			$status = 'approved';
+		}else
+		{
+			 $status = 'pending';
+		}
+       
+        if ( isset( $_REQUEST['action'] ) && 'createuser' == $_REQUEST['action'] ) {
+            $status = 'approved';
+        }
+        update_user_meta( $user_id, 'mim_user_status', $status );
+ }
+ 
+ 
+add_filter( 'manage_users_columns',  'mim_add_column'  );
+add_filter( 'manage_users_custom_column',  'mim_status_column', 10, 3 );
+
+/**
+* Added custom column in user list.
+*
+* Function Name: mim_add_column.
+*
+* @created by {Nilesh Mokani} and {12-12-2013}
+*
+**/
+
+function mim_add_column( $columns ) {
+        $the_columns['mim_user_status'] = __( 'Status', 'mim-issue' );
+
+        $newcol = array_slice( $columns, 0, -1 );
+        $newcol = array_merge( $newcol, $the_columns );
+        $columns = array_merge( $newcol, array_slice( $columns, 1 ) );
+
+        return $columns;
+}
+	
+/**
+*  Added custom column in user list.
+*
+* Function Name: mim_status_column.
+*
+* @created by {Nilesh Mokani} and {12-12-2013}
+*
+**/
+function mim_status_column( $val, $column_name, $user_id ) {
+    switch ( $column_name ) {
+        case 'mim_user_status' :
+            return get_the_author_meta('mim_user_status', $user_id);
+            break;
+
+        default:
+    }
+
+    return $val;
+}	
+
+/**
+*  Added user restriction by user status.
+*
+* Function Name: mim_authenticate_user.
+*
+* @created by {Nilesh Mokani} and {12-12-2013}
+*
+**/
+
+add_filter( 'wp_authenticate_user',	'mim_authenticate_user'  );
+
+function mim_authenticate_user( $userdata ) {
+	
+        $status = get_the_author_meta('mim_user_status', $userdata->ID);
+		
+        if ( empty( $status ) ) {
+            return $userdata;
+        }
+		
+		$message = false;
+        switch ( $status ) {
+            case 'pending':
+                $pending_message = __( '<strong>ERROR</strong>: Your account is still pending approval.', 'mim-issue' );
+                $message = new WP_Error( 'pending_approval', $pending_message );
+                break;
+            case 'approved':
+                $message = $userdata;
+                break;
+        }
+
+        return $message;
+    }
+
+/**
+*  Send mail when user registration.
+*
+* Function Name: mim_request_admin_approval_email.
+*
+* @created by {Nilesh Mokani} and {12-12-2013}
+*
+**/
+
+add_action( 'register_post', 'mim_request_admin_approval_email' , 10, 3 );
+
+function mim_request_admin_approval_email( $user_login, $user_email, $errors ) {
+        if ( $errors->get_error_code() ) {
+            return $errors;
+        }
+		
+		$message  = __( 'USERNAME (USEREMAIL) has requested a username at SITENAME', 'new-user-approve' ) . "\n\n";
+        $message .= "SITEURL\n\n";
+        $message .= __( 'To approve or deny this user access to SITENAME go to', 'new-user-approve' ) . "\n\n";
+        $message .= "ADMINURL\n\n";
+        $admin_url = admin_url(); 
+        $blogname = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
+   
+        $message = str_replace( 'USERNAME', $user_login, $message );
+        $message = str_replace( 'USEREMAIL', $user_email, $message );
+        $message = str_replace( 'SITENAME', $blogname, $message );
+        $message = str_replace( 'SITEURL', get_option( 'siteurl' ), $message );
+        $message = str_replace( 'ADMINURL', $admin_url, $message );
+
+       
+
+        $subject = sprintf( __( '[%s] User Approval', 'new-user-approve' ), $blogname );
+        
+
+        $to = get_option( 'admin_email' );
+		
+		 $admin_email = get_option( 'admin_email' );
+        if ( empty( $admin_email ) )
+            $admin_email = 'support@' . $_SERVER['SERVER_NAME'];
+
+        $from_name = get_option( 'blogname' );
+
+        $headers = array(
+            "From: \"{$from_name}\" <{$admin_email}>\n",
+            "Content-Type: text/plain; charset=\"" . get_option( 'blog_charset' ) . "\"\n",
+        );
+		
+        // send the mail
+        wp_mail( $to, $subject, $message, $headers );
+}	
+?>
